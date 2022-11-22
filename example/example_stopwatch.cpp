32499121a621351a61b636386ecf265dc9055760
Enrico Fraccaroli <enry.frak@gmail.com>
2022-11-22 16:33:59 -0500

    Add support pre and post C++11

--- example/example_stopwatch.cpp
+++ example/example_stopwatch.cpp
@@ -1,25 +1,37 @@
 #include "stopwatch/stopwatch.hpp"
 
 #include <vector>
-#include <random>
 
-inline auto generate_random_values(unsigned size)
+#if __cplusplus < 201103L
+#include <cstdlib>
+#else
+#include <random>
+#endif
+
+inline std::vector<double> generate_random_values(unsigned size)
 {
     std::vector<double> values(size);
+#if __cplusplus < 201103L
+    std::srand(10);
+    for (std::size_t i = 0; i < values.size(); ++i)
+        values[i] = -100 + std::rand() % 200;
+#else
     std::uniform_real_distribution<double> dist(-100, +100);
     std::default_random_engine eng;
     for (std::size_t i = 0; i < values.size(); ++i)
         values[i] = dist(eng);
+#endif
     return values;
 }
 
-inline auto compute_abs(std::vector<double> &values)
+inline std::vector<double> &compute_abs(std::vector<double> &values)
 {
     for (std::size_t i = 0; i < values.size(); ++i)
         values[i] = std::abs(values[i]);
+    return values;
 }
 
-inline auto compute_mean(std::vector<double> &values)
+inline double compute_mean(std::vector<double> &values)
 {
     double mean = 0.;
     for (std::size_t i = 0; i < values.size(); ++i)
@@ -34,24 +46,67 @@ int main(int, char *[])
     const unsigned size = 10000;
     const unsigned runs = 1000;
 
+#if __cplusplus < 201103L
+    sw.reset();
+    values = generate_random_values(size);
+    sw.round();
+#else
+    stopwatch::time(sw, [&] { values = generate_random_values(size); });
+#endif
     sw.set_print_mode(stopwatch::human);
-    std::cout << "Generate : " << stopwatch::time(sw, [&] { values = generate_random_values(size); }) << "\n";
-
+    std::cout << "Generate : " << sw << "\n";
     sw.set_print_mode(stopwatch::numeric);
-    std::cout << "Abs      : " << stopwatch::time(sw, [&values] { compute_abs(values); }) << "\n";
-
+    std::cout << "Generate : " << sw << "\n";
     sw.set_print_mode(stopwatch::total);
-    std::cout << "Mean     : " << stopwatch::time(sw, [&values] { compute_mean(values); }) << "\n";
+    std::cout << "Generate : " << sw << "\n";
 
-    std::cout << "Average:\n";
+#if __cplusplus < 201103L
+    sw.reset();
+    values = compute_abs(values);
+    sw.round();
+#else
+    stopwatch::time(sw, [&] { values = compute_abs(values); });
+#endif
 
     sw.set_print_mode(stopwatch::human);
-    std::cout << "  Generate : " << stopwatch::ntimes<runs>(sw, [&] { values = generate_random_values(size); }).mean() << "\n";
-
+    std::cout << "Abs      : " << sw << "\n";
     sw.set_print_mode(stopwatch::numeric);
-    std::cout << "  Abs      : " << stopwatch::ntimes<runs>(sw, [&values] { compute_abs(values); }).mean() << "\n";
-
+    std::cout << "Abs      : " << sw << "\n";
     sw.set_print_mode(stopwatch::total);
-    std::cout << "  Mean     : " << stopwatch::ntimes<runs>(sw, [&values] { compute_mean(values); }).mean() << "\n";
+    std::cout << "Abs      : " << sw << "\n";
+
+#if __cplusplus < 201103L
+    sw.reset();
+    compute_mean(values);
+    sw.round();
+#else
+    stopwatch::time(sw, [&] { compute_mean(values); });
+#endif
+
+    sw.set_print_mode(stopwatch::human);
+    std::cout << "Mean      : " << sw << "\n";
+    sw.set_print_mode(stopwatch::numeric);
+    std::cout << "Mean      : " << sw << "\n";
+    sw.set_print_mode(stopwatch::total);
+    std::cout << "Mean      : " << sw << "\n";
+
+    std::cout << "\nMultiple runs:\n\n";
+
+#if __cplusplus < 201103L
+    sw.reset();
+    for (std::size_t i = 0u; i < runs; ++i) {
+        values = generate_random_values(size);
+        sw.round();
+    }
+#else
+    stopwatch::ntimes<runs>(sw, [&] { values = generate_random_values(size); });
+#endif
+
+    sw.set_print_mode(stopwatch::human);
+    std::cout << "Generate      : " << sw.mean() << "\n";
+    sw.set_print_mode(stopwatch::numeric);
+    std::cout << "Generate      : " << sw.mean() << "\n";
+    sw.set_print_mode(stopwatch::total);
+    std::cout << "Generate      : " << sw.mean() << "\n";
     return 0;
 }
--- include/stopwatch/stopwatch.hpp
+++ include/stopwatch/stopwatch.hpp
@@ -7,8 +7,14 @@
 #include <iostream>
 #include <utility>
 #include <iomanip>
-#include <chrono>
 #include <vector>
+#include <sstream>
+
+#if __cplusplus < 201103L
+#include <time.h>
+#else
+#include <chrono>
+#endif
 
 namespace stopwatch
 {
@@ -20,65 +26,239 @@ enum PrintMode {
     total    ///< Total elapsed  :
 };
 
-class Duration {
-private:
-    std::chrono::duration<double> _duration;
-    PrintMode _print_mode;
+#if __cplusplus < 201103L
 
+namespace detail
+{
+
+static const unsigned long ns_per_sec = 1000000000UL;
+
+inline timespec normalize(timespec ts)
+{
+    while (ts.tv_nsec >= static_cast<long>(ns_per_sec)) {
+        ++(ts.tv_sec);
+        ts.tv_nsec -= ns_per_sec;
+    }
+    while (ts.tv_nsec <= -static_cast<long>(ns_per_sec)) {
+        --(ts.tv_sec);
+        ts.tv_nsec += ns_per_sec;
+    }
+    if (ts.tv_nsec < 0) {
+        --(ts.tv_sec);
+        ts.tv_nsec = (ns_per_sec + ts.tv_nsec);
+    }
+    return ts;
+}
+
+template <typename T>
+inline timespec ns_to_nanoseconds(const T &ns)
+{
+    timespec ts;
+    ts.tv_sec  = static_cast<time_t>(ns) / detail::ns_per_sec;
+    ts.tv_nsec = static_cast<long>(ns) % detail::ns_per_sec;
+    return detail::normalize(ts);
+}
+
+template <typename T>
+inline T ts_to_nanoseconds(const timespec &ts)
+{
+    return static_cast<T>((ts.tv_sec * detail::ns_per_sec) + ts.tv_nsec);
+}
+template <typename T>
+inline T ts_to_microseconds(const timespec &ts)
+{
+    unsigned long ns = ts_to_nanoseconds<unsigned long>(ts);
+    return static_cast<T>(ns / 1000UL);
+}
+template <typename T>
+inline T ts_to_milliseconds(const timespec &ts)
+{
+    unsigned long ns = ts_to_nanoseconds<unsigned long>(ts);
+    return static_cast<T>(ns / 1000000UL);
+}
+template <typename T>
+inline T ts_to_seconds(const timespec &ts)
+{
+    unsigned long ns = ts_to_nanoseconds<unsigned long>(ts);
+    return static_cast<T>(ns / detail::ns_per_sec);
+}
+template <typename T>
+inline T ts_to_minutes(const timespec &ts)
+{
+    unsigned long ns = ts_to_nanoseconds<unsigned long>(ts);
+    return static_cast<T>((ns / detail::ns_per_sec) / 60);
+}
+template <typename T>
+inline T ts_to_hours(const timespec &ts)
+{
+    unsigned long ns = ts_to_nanoseconds<unsigned long>(ts);
+    return static_cast<T>((ns / detail::ns_per_sec) / 3600);
+}
+
+inline void clock_gettime(timespec &ts)
+{
+    clock_gettime(CLOCK_REALTIME, &ts);
+}
+
+inline timespec clock_now()
+{
+    timespec ts;
+    clock_gettime(CLOCK_REALTIME, &ts);
+    return ts;
+}
+
+} // namespace detail
+
+inline timespec operator+(const timespec &lhs, const timespec &rhs)
+{
+    return detail::ns_to_nanoseconds(
+        detail::ts_to_nanoseconds<unsigned long>(lhs) +
+        detail::ts_to_nanoseconds<unsigned long>(rhs));
+}
+
+template <typename T>
+inline timespec operator+(const timespec &lhs, const T &rhs)
+{
+    return detail::ns_to_nanoseconds(detail::ts_to_nanoseconds<T>(lhs) + rhs);
+}
+
+inline timespec operator-(const timespec &lhs, const timespec &rhs)
+{
+    return detail::ns_to_nanoseconds(
+        detail::ts_to_nanoseconds<unsigned long>(lhs) -
+        detail::ts_to_nanoseconds<unsigned long>(rhs));
+}
+
+template <typename T>
+inline timespec operator-(const timespec &lhs, const T &rhs)
+{
+    return detail::ns_to_nanoseconds(detail::ts_to_nanoseconds<T>(lhs) - rhs);
+}
+
+inline timespec operator/(const timespec &lhs, const timespec &rhs)
+{
+    return detail::ns_to_nanoseconds(
+        detail::ts_to_nanoseconds<unsigned long>(lhs) /
+        detail::ts_to_nanoseconds<unsigned long>(rhs));
+}
+
+template <typename T>
+inline timespec operator/(const timespec &lhs, const T &rhs)
+{
+    return detail::ns_to_nanoseconds(detail::ts_to_nanoseconds<T>(lhs) / rhs);
+}
+
+inline timespec &operator+=(timespec &lhs, const timespec &rhs)
+{
+    return (lhs = lhs + rhs);
+}
+
+template <typename T>
+inline timespec &operator+=(timespec &lhs, const T &rhs)
+{
+    return (lhs = lhs + rhs);
+}
+
+inline timespec &operator-=(timespec &lhs, const timespec &rhs)
+{
+    return (lhs = lhs - rhs);
+}
+
+template <typename T>
+inline timespec &operator-=(timespec &lhs, const T &rhs)
+{
+    return (lhs = lhs - rhs);
+}
+
+inline timespec &operator/=(timespec &lhs, const timespec &rhs)
+{
+    return (lhs = lhs / rhs);
+}
+
+template <typename T>
+inline timespec &operator/=(timespec &lhs, const T &rhs)
+{
+    return (lhs = lhs / rhs);
+}
+
+#endif
+
+class Duration {
 public:
-    Duration(std::chrono::duration<double> duration = std::chrono::duration<double>::zero(),
-             PrintMode print_mode                   = human)
+#if __cplusplus < 201103L
+    typedef timespec duration_type_t;
+#else
+    typedef std::chrono::duration<double> duration_type_t;
+#endif
+
+    Duration(duration_type_t duration, PrintMode print_mode)
         : _duration(duration),
           _print_mode(print_mode)
     {
         // Nothing to do.
     }
-    
-    inline auto operator+(const Duration &rhs) const
+
+    static inline duration_type_t zero()
+    {
+#if __cplusplus < 201103L
+        duration_type_t duration;
+        duration.tv_sec  = 0;
+        duration.tv_nsec = 0;
+        return duration;
+#else
+        return duration_type_t::zero();
+#endif
+    }
+
+    inline void set_print_mode(PrintMode print_mode)
+    {
+        _print_mode = print_mode;
+    }
+
+    inline Duration operator+(const Duration &rhs) const
     {
         Duration new_duration = *this;
         new_duration._duration += rhs._duration;
         return new_duration;
     }
 
-    template<typename T>
-    inline auto operator+(const T &rhs) const
+    template <typename T>
+    inline Duration operator+(const T &rhs) const
     {
         Duration new_duration = *this;
         new_duration._duration += rhs;
         return new_duration;
     }
 
-    template<typename T>
-    inline auto operator/(const T &rhs) const
+    template <typename T>
+    inline Duration operator/(const T &rhs) const
     {
         Duration new_duration = *this;
         new_duration._duration /= rhs;
         return new_duration;
     }
 
-
-    inline auto operator+=(const Duration &rhs)
+    inline Duration &operator+=(const Duration &rhs)
     {
         _duration += rhs._duration;
         return *this;
     }
 
-    template<typename T>
-    inline auto operator+=(const T &rhs)
+    template <typename T>
+    inline Duration &operator+=(const T &rhs)
     {
         _duration += rhs;
         return *this;
     }
 
-    template<typename T>
-    inline auto operator/=(const T &rhs)
+    template <typename T>
+    inline Duration &operator/=(const T &rhs)
     {
         _duration /= rhs;
         return *this;
     }
 
-    inline auto operator=(const std::chrono::duration<double> &rhs)
+    inline Duration &operator=(const duration_type_t &rhs)
     {
         _duration = rhs;
         return *this;
@@ -88,8 +268,40 @@ public:
     {
         std::stringstream ss;
         // Get the duration.
-        auto duration = _duration;
+        duration_type_t duration = _duration;
         if (_print_mode == human) {
+#if __cplusplus < 201103L
+            unsigned h = detail::ts_to_hours<unsigned>(duration);
+            if (h) {
+                ss << std::setw(3) << h << "H ";
+                duration -= (h * 3600) * detail::ns_per_sec;
+            }
+            unsigned m = detail::ts_to_minutes<unsigned>(duration);
+            if (m) {
+                ss << std::setw(3) << m << "M ";
+                duration -= (m * 60) * detail::ns_per_sec;
+            }
+            unsigned s = detail::ts_to_seconds<unsigned>(duration);
+            if (s) {
+                ss << std::setw(3) << s << "s ";
+                duration -= s * detail::ns_per_sec;
+            }
+            unsigned ms = detail::ts_to_milliseconds<unsigned>(duration);
+            if (ms) {
+                ss << std::setw(3) << ms << "m ";
+                duration -= ms * 1000000UL;
+            }
+            unsigned us = detail::ts_to_microseconds<unsigned>(duration);
+            if (us) {
+                ss << std::setw(3) << us << "u ";
+                duration -= us * 1000UL;
+            }
+            unsigned ns = detail::ts_to_nanoseconds<unsigned>(duration);
+            if (ns) {
+                ss << std::setw(3) << ns << "n ";
+                duration -= ns;
+            }
+#else
             auto h = std::chrono::duration_cast<std::chrono::hours>(duration);
             if (h.count()) {
                 ss << std::setw(3) << h.count() << "H ";
@@ -120,7 +332,28 @@ public:
                 ss << std::setw(3) << ns.count() << "n ";
                 duration -= ns;
             }
+#endif
         } else if (_print_mode == numeric) {
+#if __cplusplus < 201103L
+            unsigned h = detail::ts_to_hours<unsigned>(duration);
+            ss << h << ".";
+            duration -= (h * 3600) * detail::ns_per_sec;
+            unsigned m = detail::ts_to_minutes<unsigned>(duration);
+            ss << m << ".";
+            duration -= (m * 60) * detail::ns_per_sec;
+            unsigned s = detail::ts_to_seconds<unsigned>(duration);
+            ss << s << ".";
+            duration -= s * detail::ns_per_sec;
+            unsigned ms = detail::ts_to_milliseconds<unsigned>(duration);
+            ss << ms << ".";
+            duration -= ms * 1000000UL;
+            unsigned us = detail::ts_to_microseconds<unsigned>(duration);
+            ss << us << ".";
+            duration -= us * 1000UL;
+            unsigned ns = detail::ts_to_nanoseconds<unsigned>(duration);
+            ss << ns << ".";
+            duration -= ns;
+#else
             auto h = std::chrono::duration_cast<std::chrono::hours>(duration);
             ss << h.count() << ".";
             duration -= h;
@@ -138,9 +371,14 @@ public:
             duration -= us;
             auto ns = std::chrono::duration_cast<std::chrono::nanoseconds>(duration);
             ss << ns.count();
+#endif
             duration -= ns;
         } else {
+#if __cplusplus < 201103L
+            ss << detail::ts_to_nanoseconds<double>(duration) * 1e-09;
+#else
             ss << (static_cast<double>(std::chrono::duration_cast<std::chrono::nanoseconds>(duration).count()) * 1e-09);
+#endif
         }
         return ss.str();
     }
@@ -150,59 +388,90 @@ public:
         lhs << rhs.to_string();
         return lhs;
     }
+
+private:
+    duration_type_t _duration;
+    PrintMode _print_mode;
 };
 
 class Stopwatch {
-private:
-    std::chrono::high_resolution_clock::time_point _last_time_point;
-    Duration _total_duration;
-    std::vector<Duration> _partials;
-    PrintMode _print_mode;
-
 public:
+#if __cplusplus < 201103L
+    typedef timespec clock_type_t;
+    typedef timespec time_point_type_t;
+#else
+    typedef std::chrono::high_resolution_clock clock_type_t;
+    typedef clock_type_t::time_point time_point_type_t;
+#endif
+
     Stopwatch(PrintMode print_mode = human)
-        : _last_time_point(std::chrono::high_resolution_clock::now()),
-          _total_duration(),
+        : _last_time_point(),
+          _total_duration(Duration::zero(), print_mode),
           _partials(),
           _print_mode(print_mode)
     {
-        // Nothing to do.
+#if __cplusplus < 201103L
+        detail::clock_gettime(_last_time_point);
+#else
+        _last_time_point                 = clock_type_t::now();
+#endif
     }
 
     inline void set_print_mode(PrintMode print_mode)
     {
         _print_mode = print_mode;
+        _total_duration.set_print_mode(print_mode);
     }
 
     inline void reset()
     {
-        _last_time_point = std::chrono::high_resolution_clock::now();
-        _total_duration  = std::chrono::duration<double>::zero();
+#if __cplusplus < 201103L
+        _total_duration = Duration(Duration::zero(), _print_mode);
+#else
+        _total_duration                  = std::chrono::duration<double>::zero();
+#endif
         _partials.clear();
+        // Star again the timer.
+        this->start();
     }
 
     inline void start()
     {
-        _last_time_point = std::chrono::high_resolution_clock::now();
+#if __cplusplus < 201103L
+        detail::clock_gettime(_last_time_point);
+#else
+        _last_time_point                 = clock_type_t::now();
+#endif
     }
 
     inline void round()
     {
-        auto now         = std::chrono::high_resolution_clock::now();
-        auto elapsed     = now - _last_time_point;
+#if __cplusplus < 201103L
+        time_point_type_t now     = detail::clock_now();
+        time_point_type_t elapsed = now - _last_time_point;
+#else
+        time_point_type_t now            = clock_type_t::now();
+        std::chrono::nanoseconds elapsed = now - _last_time_point;
+#endif
         _last_time_point = now;
         _total_duration += elapsed;
-        _partials.emplace_back(elapsed);
+        _partials.push_back(Duration(elapsed, _print_mode));
     }
 
-    auto last_round() const
+    Duration last_round() const
     {
-        if (_partials.empty())
-            return Duration(std::chrono::high_resolution_clock::now() - _last_time_point);
+        if (_partials.empty()) {
+#if __cplusplus < 201103L
+            time_point_type_t now = detail::clock_now();
+#else
+            time_point_type_t now = clock_type_t::now();
+#endif
+            return Duration(now - _last_time_point, _print_mode);
+        }
         return _partials.back();
     }
 
-    auto total() const
+    Duration total() const
     {
         return _total_duration;
     }
@@ -215,7 +484,7 @@ public:
         return sw;
     }
 
-    inline auto partials() const
+    inline std::vector<Duration> partials() const
     {
         return _partials;
     }
@@ -225,11 +494,31 @@ public:
         return _total_duration.to_string();
     }
 
+    inline Duration &operator[](std::size_t position)
+    {
+        if (position < _partials.size())
+            return _partials[position];
+        throw std::out_of_range("Out of range of partial times.");
+    }
+
+    inline const Duration &operator[](std::size_t position) const
+    {
+        if (position < _partials.size())
+            return _partials[position];
+        throw std::out_of_range("Out of range of partial times.");
+    }
+
     friend std::ostream &operator<<(std::ostream &lhs, const Stopwatch &rhs)
     {
         lhs << rhs.to_string();
         return lhs;
     }
+
+private:
+    time_point_type_t _last_time_point;
+    Duration _total_duration;
+    std::vector<Duration> _partials;
+    PrintMode _print_mode;
 };
 
 /// @brief Runs the function and samples the elapsed time.
@@ -237,10 +526,9 @@ public:
 /// @param function the function to sample.
 /// @return the same stopwatch passed as argument.
 template <class Function>
-inline auto &time(Stopwatch &stopwatch, Function &&function)
+inline Stopwatch &time(Stopwatch &stopwatch, const Function &function)
 {
     stopwatch.reset();
-    stopwatch.start();
     function();
     stopwatch.round();
     return stopwatch;
@@ -251,11 +539,10 @@ inline auto &time(Stopwatch &stopwatch, Function &&function)
 /// @param function the function to sample.
 /// @return the same stopwatch passed as argument.
 template <std::size_t N, class Function>
-inline auto ntimes(Stopwatch &stopwatch, Function &&function)
+inline Stopwatch &ntimes(Stopwatch &stopwatch, const Function &function)
 {
     stopwatch.reset();
     for (std::size_t i = 0u; i < N; ++i) {
-        stopwatch.start();
         function();
         stopwatch.round();
     }
