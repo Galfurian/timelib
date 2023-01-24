a82230c9ce5897cf3d09d9ba95c5337302674295
Enrico Fraccaroli (Galfurian) <enry.frak@gmail.com>
2023-01-24 15:08:39 -0500

    Separate the example into C++98 and C++11 version.

--- example/example_cpp11.cpp
+++ example/example_cpp11.cpp
@@ -1,26 +1,15 @@
 #include "stopwatch/stopwatch.hpp"
 
 #include <vector>
-
-#if __cplusplus < 201103L
-#include <cstdlib>
-#else
 #include <random>
-#endif
 
 inline std::vector<double> generate_random_values(unsigned size)
 {
     std::vector<double> values(size);
-#if __cplusplus < 201103L
-    std::srand(10);
-    for (std::size_t i = 0; i < values.size(); ++i)
-        values[i] = -100 + std::rand() % 200;
-#else
     std::uniform_real_distribution<double> dist(-100, +100);
     std::default_random_engine eng;
     for (std::size_t i = 0; i < values.size(); ++i)
         values[i] = dist(eng);
-#endif
     return values;
 }
 
@@ -46,13 +35,8 @@ int main(int, char *[])
     const unsigned size = 10000;
     const unsigned runs = 1000;
 
-#if __cplusplus < 201103L
-    sw.reset();
-    values = generate_random_values(size);
-    sw.round();
-#else
     stopwatch::time(sw, [&] { values = generate_random_values(size); });
-#endif
+
     sw.set_print_mode(stopwatch::human);
     std::cout << "Generate : " << sw << "\n";
     sw.set_print_mode(stopwatch::numeric);
@@ -60,13 +44,7 @@ int main(int, char *[])
     sw.set_print_mode(stopwatch::total);
     std::cout << "Generate : " << sw << "\n";
 
-#if __cplusplus < 201103L
-    sw.reset();
-    values = compute_abs(values);
-    sw.round();
-#else
     stopwatch::time(sw, [&] { values = compute_abs(values); });
-#endif
 
     sw.set_print_mode(stopwatch::human);
     std::cout << "Abs      : " << sw << "\n";
@@ -75,13 +53,7 @@ int main(int, char *[])
     sw.set_print_mode(stopwatch::total);
     std::cout << "Abs      : " << sw << "\n";
 
-#if __cplusplus < 201103L
-    sw.reset();
-    compute_mean(values);
-    sw.round();
-#else
     stopwatch::time(sw, [&] { compute_mean(values); });
-#endif
 
     sw.set_print_mode(stopwatch::human);
     std::cout << "Mean      : " << sw << "\n";
@@ -92,17 +64,7 @@ int main(int, char *[])
 
     std::cout << "\nMultiple runs:\n\n";
 
-#if __cplusplus < 201103L
-    sw.reset();
-    for (std::size_t i = 0u; i < runs; ++i) {
-        values = generate_random_values(size);
-        sw.round();
-    }
-#else
     stopwatch::ntimes<runs>(sw, [&] { values = generate_random_values(size); });
-#endif
-
-    std::cout << 60 * 1000000000UL << "\n";
 
     sw.set_print_mode(stopwatch::human);
     std::cout << "Generate      : " << sw.mean() << "\n";
---
+++ example/example_cpp98.cpp
@@ -0,0 +1,91 @@
+#include "stopwatch/stopwatch.hpp"
+
+#include <cstdlib>
+#include <vector>
+
+inline std::vector<double> generate_random_values(unsigned size)
+{
+    std::vector<double> values(size);
+    std::srand(10);
+    for (std::size_t i = 0; i < values.size(); ++i)
+        values[i] = -100 + std::rand() % 200;
+    return values;
+}
+
+inline std::vector<double> &compute_abs(std::vector<double> &values)
+{
+    for (std::size_t i = 0; i < values.size(); ++i)
+        values[i] = std::abs(values[i]);
+    return values;
+}
+
+inline double compute_mean(std::vector<double> &values)
+{
+    double mean = 0.;
+    for (std::size_t i = 0; i < values.size(); ++i)
+        mean += values[i];
+    return mean / static_cast<double>(values.size());
+}
+
+int main(int, char *[])
+{
+    stopwatch::Stopwatch sw;
+    std::vector<double> values;
+    const unsigned size = 10000;
+    const unsigned runs = 1000;
+
+    sw.reset();
+    values = generate_random_values(size);
+    sw.round();
+
+    sw.set_print_mode(stopwatch::human);
+    std::cout << "Generate : " << sw << "\n";
+    sw.set_print_mode(stopwatch::numeric);
+    std::cout << "Generate : " << sw << "\n";
+    sw.set_print_mode(stopwatch::total);
+    std::cout << "Generate : " << sw << "\n";
+
+    sw.reset();
+    values = compute_abs(values);
+    sw.round();
+
+    sw.set_print_mode(stopwatch::human);
+    std::cout << "Abs      : " << sw << "\n";
+    sw.set_print_mode(stopwatch::numeric);
+    std::cout << "Abs      : " << sw << "\n";
+    sw.set_print_mode(stopwatch::total);
+    std::cout << "Abs      : " << sw << "\n";
+
+    sw.reset();
+    compute_mean(values);
+    sw.round();
+
+    sw.set_print_mode(stopwatch::human);
+    std::cout << "Mean      : " << sw << "\n";
+    sw.set_print_mode(stopwatch::numeric);
+    std::cout << "Mean      : " << sw << "\n";
+    sw.set_print_mode(stopwatch::total);
+    std::cout << "Mean      : " << sw << "\n";
+
+    std::cout << "\nMultiple runs:\n\n";
+
+    sw.reset();
+    for (std::size_t i = 0u; i < runs; ++i) {
+        values = generate_random_values(size);
+        sw.round();
+    }
+
+    sw.set_print_mode(stopwatch::human);
+    std::cout << "Generate      : " << sw.mean() << "\n";
+    sw.set_print_mode(stopwatch::numeric);
+    std::cout << "Generate      : " << sw.mean() << "\n";
+    sw.set_print_mode(stopwatch::total);
+    std::cout << "Generate      : " << sw.mean() << "\n";
+
+    std::cout << "\n";
+    sw.set_print_mode(stopwatch::custom);
+    sw.set_format("%H:%M:%s:%m:%u:%n");
+    std::cout << "Generate      : " << sw.mean() << "\n";
+
+    return 0;
+}
